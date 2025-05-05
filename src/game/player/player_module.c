b29d2535f48b696e313fcd98d9bdad2578a3fb52
LuizRedGalaxy <luizgustavoglt@gmail.com>
2025-05-05 19:45:17 -0300

    Adicionado subir e descer nas escadas

--- src/game/player/player_module.c
+++ src/game/player/player_module.c
@@ -16,6 +16,19 @@ typedef enum
   GPA_CLIMBING = 6,
 } GamePlayerAnimations;
 
+typedef enum
+{
+  FN_ONE = 0,
+  FN_TWO,
+  FN_THREE,
+  FN_FOUR
+} FloorName;
+
+typedef struct
+{
+  uint16_t start, end;
+} Stair;
+
 // ### START Variaveis globais ###
 
 static Sprite *_sprite;
@@ -26,6 +39,9 @@ static u16 _left_x, _right_x, _bottom_y, _top_y;
 static u8 _frame_counter = 0;
 static bool _turn_complete = false;
 static bool _first_entry = true;
+static Stair _stairs[3][3];
+static FloorName _current_floor = FN_ONE;
+static u16 _floor_limit[4] = {160, 128, 96, 64};
 // ### END Variaveis globais ###
 
 // ### START Funções internas ###
@@ -33,6 +49,7 @@ static bool _first_entry = true;
 static void set_state(GamePlayerStates state);
 static void convert_positions_fix_to_ints();
 static bool can_climb_up();
+static bool can_climb_down();
 
 static void running_right_logic(const GameInputs *inputs);
 static void stoped_right_logic(const GameInputs *inputs);
@@ -54,6 +71,26 @@ void player_setup()
   PAL_setPalette(PAL1, spr_player.palette->data, DMA);
   _sprite = SPR_addSprite(&spr_player, 128, 176 - 16, TILE_ATTR_FULL(PAL1, false, false, false, 1));
   set_state(GPS_RUNNING_RIGHT);
+  _stairs[FN_ONE][0].start = 104;
+  _stairs[FN_ONE][0].end = 111;
+  _stairs[FN_ONE][1].start = 200;
+  _stairs[FN_ONE][1].end = 207;
+  _stairs[FN_ONE][2].start = 300;
+  _stairs[FN_ONE][2].end = 300;
+
+  _stairs[FN_TWO][0].start = 88;
+  _stairs[FN_TWO][0].end = 95;
+  _stairs[FN_TWO][1].start = 216;
+  _stairs[FN_TWO][1].end = 223;
+  _stairs[FN_TWO][2].start = 152;
+  _stairs[FN_TWO][2].end = 159;
+
+  _stairs[FN_THREE][0].start = 104;
+  _stairs[FN_THREE][0].end = 111;
+  _stairs[FN_THREE][1].start = 200;
+  _stairs[FN_THREE][1].end = 204;
+  _stairs[FN_THREE][2].start = 300;
+  _stairs[FN_THREE][2].end = 300;
 }
 
 /**
@@ -174,24 +211,54 @@ static bool can_climb_up()
 {
   convert_positions_fix_to_ints();
 
-  if (_right_x >= 104 && _right_x <= 111)
+  if (_current_floor == FN_FOUR)
   {
-    return true;
+    return false;
   }
 
-  if (_left_x >= 104 && _left_x <= 111)
+  for (u8 i = 0; i < 3; i++)
   {
-    return true;
+    if (_right_x >= _stairs[_current_floor][i].start + 2 && _right_x <= _stairs[_current_floor][i].end - 2)
+    {
+      _x = intToFix16(_stairs[_current_floor][i].start);
+      return true;
+    }
+
+    if (_left_x >= _stairs[_current_floor][i].start + 2 && _left_x <= _stairs[_current_floor][i].end - 2)
+    {   
+      _x = intToFix16(_stairs[_current_floor][i].start);
+      return true;
+    }
   }
 
-  if (_right_x >= 200 && _right_x <= 207)
+  return false;
+}
+
+/**
+ * Verifica se o player pode subir alguma escada
+ */
+static bool can_climb_down()
+{
+  convert_positions_fix_to_ints();
+
+  if (_current_floor == FN_ONE)
   {
-    return true;
+    return false;
   }
 
-  if (_left_x >= 200 && _left_x <= 207)
+  for (u8 i = 0; i < 3; i++)
   {
-    return true;
+    if (_right_x >= _stairs[_current_floor - 1][i].start + 2 && _right_x <= _stairs[_current_floor - 1][i].end - 2)
+    {
+      _x = intToFix16(_stairs[_current_floor - 1][i].start);
+      return true;
+    }
+
+    if (_left_x >= _stairs[_current_floor - 1][i].start + 2 && _left_x <= _stairs[_current_floor - 1][i].end - 2)
+    {
+      _x = intToFix16(_stairs[_current_floor - 1][i].start);
+      return true;
+    }
   }
 
   return false;
@@ -213,9 +280,15 @@ static void running_right_logic(const GameInputs *inputs)
     return;
   }
 
-  if (game_inputs_click(inputs->up) && can_climb_up())
+  if (game_inputs_hold(inputs->up) && can_climb_up())
   {
     set_state(GPS_CLIMBING_UP);
+    return;
+  }
+
+  if (game_inputs_hold(inputs->down) && can_climb_down())
+  {
+    set_state(GPS_CLIMBING_DOWN);
     return;
   }
 
@@ -283,6 +356,18 @@ static void running_left_logic(const GameInputs *inputs)
     return;
   }
 
+  if (game_inputs_hold(inputs->up) && can_climb_up())
+  {
+    set_state(GPS_CLIMBING_UP);
+    return;
+  }
+
+  if (game_inputs_hold(inputs->down) && can_climb_down())
+  {
+    set_state(GPS_CLIMBING_DOWN);
+    return;
+  }
+
   if (fix16ToInt(_x) > 80)
   {
     _x -= FIX16(0.9);
@@ -341,12 +426,14 @@ static void climbing_up_logic(const GameInputs *inputs)
     SPR_setAnim(_sprite, GPA_CLIMBING);
   }
 
-  if (fix16ToInt(_y) > 128)
+  if (fix16ToInt(_y) > _floor_limit[_current_floor + 1])
   {
     _y -= FIX16(1);
     SPR_setPosition(_sprite, fix16ToInt(_x), fix16ToInt(_y));
     return;
   }
+
+  _current_floor = _current_floor + 1;
 
   set_state(_last_state);
   return;
@@ -361,4 +448,16 @@ static void climbing_down_logic(const GameInputs *inputs)
   {
     SPR_setAnim(_sprite, GPA_CLIMBING);
   }
+
+  if (fix16ToInt(_y) < _floor_limit[_current_floor - 1])
+  {
+    _y += FIX16(1);
+    SPR_setPosition(_sprite, fix16ToInt(_x), fix16ToInt(_y));
+    return;
+  }
+
+  _current_floor = _current_floor - 1;
+
+  set_state(_last_state);
+  return;
 }
