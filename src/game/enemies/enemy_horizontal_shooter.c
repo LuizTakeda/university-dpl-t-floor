d3e8aabd91b638793c09651d91466e58657ff71f
LuizRedGalaxy <luizgustavoglt@gmail.com>
2025-06-21 19:15:43 -0300

    feat(horizontal-shooter): add spawn & cleanup logic; adjust sprite animation speed

--- src/game/enemies/enemy_horizontal_shooter.c
+++ src/game/enemies/enemy_horizontal_shooter.c
@@ -7,7 +7,8 @@
 //  Defines
 //**************************************************
 
-#define SHOTTERS_LIMIT 4
+#define LIMIT 4
+#define SPAWN_RATE 100
 
 //**************************************************
 //  Enums
@@ -17,11 +18,13 @@
 //  Static Functions
 //**************************************************
 
+static void frame_change(Sprite *sprite);
+
 //**************************************************
 //  Globals
 //**************************************************
 
-static Enemy _shotters[SHOTTERS_LIMIT];
+static Enemy _shotters[LIMIT];
 static u16 **_sprite_indexes;
 static u16 _alive_quantity;
 static u16 _spawn_countdown;
@@ -39,18 +42,77 @@ void enemy_horizontal_shooter_setup()
   _alive_quantity = 0;
   _spawn_countdown = 0;
 
-  for (int i = 0; i < SHOTTERS_LIMIT; i++)
+  for (int i = 0; i < LIMIT; i++)
   {
     _shotters[i].dead = true;
   }
 }
 
-void enemy_horizontal_shooter_clean() {
-  
+void enemy_horizontal_shooter_clean()
+{
+  _alive_quantity = 0;
+
+  for (u8 i = 0; i < LIMIT; i++)
+  {
+    if (_shotters[i].dead)
+    {
+      continue;
+    }
+
+    _shotters[i].dead = true;
+    SPR_releaseSprite(_shotters[i]._sprite);
+  }
+
+  SPR_defragVRAM();
 }
 
 bool enemy_horizontal_shooter_spawn(GameLevel game_level)
 {
+  if (_alive_quantity >= LIMIT)
+  {
+    return false;
+  }
+
+  if (_spawn_countdown > 0)
+  {
+    _spawn_countdown--;
+    return false;
+  }
+
+  _spawn_countdown = SPAWN_RATE;
+
+  // Find the dead enemy position
+  int i;
+  for (i = 0; i < LIMIT && !_shotters[i].dead; i++)
+    ;
+
+  _shotters[i].dead = false;
+
+  _shotters[i].x = FIX16(96 + (16 * (random() % 7)));
+  _shotters[i].y = FIX16(55);
+
+  _shotters[i].data = 0;
+  _shotters[i].state = ENEMY_STATE_SPAWNING;
+
+  _shotters[i]
+      ._sprite = SPR_addSprite(
+      &spr_enemy_04,
+      fix16ToInt(_shotters[i].x), fix16ToInt(_shotters[i].y),
+      TILE_ATTR(PAL3, 0, false, false));
+
+  _shotters[i].hit_box_left_x = fix16ToInt(_shotters[i].x);
+  _shotters[i].hit_box_right_x = fix16ToInt(_shotters[i].x) + _shotters[i]._sprite->definition->w - 1;
+
+  _shotters[i].hit_box_top_y = fix16ToInt(_shotters[i].y);
+  _shotters[i].hit_box_bottom_y = fix16ToInt(_shotters[i].y) + _shotters[i]._sprite->definition->h - 1 - 3;
+
+  SPR_setAutoTileUpload(_shotters[i]._sprite, FALSE);
+  SPR_setFrameChangeCallback(_shotters[i]._sprite, &frame_change);
+
+  _alive_quantity++;
+
+  return true;
+
   return false;
 }
 
@@ -60,7 +122,7 @@ EnemiesEvents enemy_horizontal_shooter_logic(const GamePlayerInfo *player_info)
       .enemies_dead = 0,
       .player_hit = false};
 
-  for (int i = i; i < SHOTTERS_LIMIT; i++)
+  for (int i = 0; i < LIMIT; i++)
   {
     Enemy *enemy = &_shotters[i];
 
@@ -71,6 +133,20 @@ EnemiesEvents enemy_horizontal_shooter_logic(const GamePlayerInfo *player_info)
 
     switch (enemy->state)
     {
+    case ENEMY_STATE_SPAWNING:
+      SPR_setAnim(enemy->_sprite, 0);
+
+      if (enemy->_sprite->frameInd >= 4)
+      {
+        enemy->state = ENEMY_STATE_SHOOTING;
+      }
+
+      break;
+
+    case ENEMY_STATE_SHOOTING:
+      SPR_setAnim(enemy->_sprite, 1);
+      break;
+
     default:
       break;
     }
